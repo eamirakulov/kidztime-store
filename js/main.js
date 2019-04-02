564df9cba0f4a3ee800a3a993de63f73bc621a1b
eamirakulov <emil@amirakulov.com>
2019-04-03 00:36:05 +0600

    Payment proccess

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(function() {
+	$('.img-feed').slick({
+		slidesToShow: 6,
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		infinite: true,
+		responsive: [
+	    {
+	      breakpoint: 767,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        centermode: true
+	      }
+	    }]
+	});
+
+	$('.qty-input .plus').click(function() {
+		var value = parseInt($(this).parent().find('input').val());
+		$(this).parent().find('input').val(value+1);
+	});
+	$('.qty-input .minus').click(function() {
+		var value = parseInt($(this).parent().find('input').val());
+		if(value > 1) {
+			$(this).parent().find('input').val(value - 1);
+		}
+	});
+	$('.checkout-btn').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		modal: true
+	}).click();
+
+	$('.close-modal').click(function(e) {
+		e.preventDefault();
+		var magnificPopup = $.magnificPopup.instance; 
+		magnificPopup.close(); 
+	});
+	$('.go-guest').click(function(e) {
+		e.preventDefault();
+		$('#modal .options').fadeOut(function() {
+			$('#modal .guest-checkout').fadeIn();
+		});
+	});
+	$('h2.back').click(function(e) {
+		e.preventDefault();
+		$('#modal .guest-checkout').fadeOut(function() {
+			$('#modal .options').fadeIn();
+		});
+	});
+
+});
